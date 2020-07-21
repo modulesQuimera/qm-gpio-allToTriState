f2b977cc5db41f94e1cad31d6c99ba5deb88214b
Ian Alves <igaps.eng17@uea.edu.br>
2020-07-21 15:21:14 -0400

    update

--- AllToTriState.js
+++ AllToTriState.js
@@ -5,33 +5,50 @@ module.exports = function(RED) {
 
     function AllToTriStateNode(config) {
         RED.nodes.createNode(this, config);
-        var node = this
         this.serial = config.serial;
         this.serialConfig = RED.nodes.getNode(this.serial);
-        node.gpionum = config.gpionum
-        node.output = config.output
+        this.gpionum = config.gpionum;
+        this.output = config.output;
+        this.slot = config.slot;
+        var node = this;
 
         node.on('input', function(msg, send, done) {
             var globalContext = node.context().global;
             var exportMode = globalContext.get("exportMode");
             var currentMode = globalContext.get("currentMode");
-            var file = globalContext.get("exportFile")
+            var file = globalContext.get("exportFile");
             var command = {
-                type: "GPIO_modular_V1.0",
-                slot: 1,
+                type: "GPIO_modular_V1_0",
+                slot: parseInt(node.slot),
                 method: "all_to_tri_state",
-            }
-            var file = globalContext.get("exportFile")
+                get_output: {},
+                compare: {}
+            };
             var slot = globalContext.get("slot");
 
-            if(currentMode == "test"){file.slots[slot].jig_test.push(command)}
-            else{file.slots[slot].jig_error.push(command)}
+            if(!(slot === "begin" || slot === "end")){
+                if(currentMode == "test"){
+                    file.slots[slot].jig_test.push(command);
+                }
+                else{
+                    file.slots[slot].jig_error.push(command);
+                }
+            }
+            else{
+                if(slot === "begin"){
+                    file.slots[0].jig_test.push(command);
+                    // file.begin.push(command);
+                }
+                else{
+                    file.slots[3].jig_test.push(command);
+                    // file.end.push(command);
+                }
+            }
             globalContext.set("exportFile", file);
-            send(msg)
-            console.log(command)
+            send(msg);
+            console.log(command);
         });
     }
-
     // nome do modulo
     RED.nodes.registerType("AllToTriState", AllToTriStateNode);
-}
+};
